f5ce974e922bbc08e4f4c9634c79784a116d25ae
Oleksandr Shepetko <a@shepetko.com>
2024-10-24 20:47:33 +0300

    Remake schematic with KiCad #3

--- schematic/genpcb.sh
+++ schematic/genpcb.sh
@@ -3,11 +3,12 @@
 mkdir -p ./gcode
 rm -f ./gcode/*.ngc
 
-# The board is single sided, currently
-# pcb2gcode --config pcb2gcode.ini --front ./gerber/Cronus-F_Cu.gbr --y-offset 0.5
-pcb2gcode --config pcb2gcode.ini --back ./gerber/Cronus-B_Cu.gbr --x-offset -71.0 --y-offset 0.5
-pcb2gcode --config pcb2gcode.ini --outline ./gerber/Cronus-Edge_Cuts.gbr --x-offset -71.0 --y-offset 0.5
-pcb2gcode --config pcb2gcode.ini --drill ./gerber/Cronus-PTH.drl --drill-output drill_1.ngc --x-offset -71.0 --y-offset 0.5
-pcb2gcode --config pcb2gcode.ini --drill ./gerber/Cronus-NPTH.drl --drill-output drill_2.ngc --x-offset -71.0 --y-offset 0.5
+pcb2gcode --config pcb2gcode.ini --back ./gerber/Cronus-B_Cu.gbr --x-offset -72.0
+
+pcb2gcode --config pcb2gcode.ini --drill ./gerber/Cronus-PTH.drl --drill-output drill_0_9mm.ngc --x-offset -72.0 --drill-feed 250
+
+pcb2gcode --config pcb2gcode.ini --drill ./gerber/Cronus-NPTH.drl --drill-output drill_2_2mm.ngc --x-offset -72.0 --drill-feed 50
+
+pcb2gcode --config pcb2gcode.ini --outline ./gerber/Cronus-Edge_Cuts.gbr --x-offset -72.0
 
 rm -f ./gcode/*.svg
